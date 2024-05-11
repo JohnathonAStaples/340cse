6ff44d2d14764fb6b3e916fe9c1d7d551041351a
Johnathon Staples <staplesjohnathona@gmail.com>
2024-05-11 16:31:27 -0600

    fixed the problem I was having

--- server.js
+++ server.js
@@ -23,9 +23,7 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 
-/* ***********************
- * Index route
- *************************/
+// Index route
 app.get("/", function(req, res){
   res.render("index", {title: "Home"})
 })
