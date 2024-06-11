1867b288339653ea15927150395b405f9c142ae7
Johnathon Staples <StapesJohnathonA@gmail.com>
2024-06-10 20:43:55 -0600

    data validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities")
+const regValidate = require('../utilities/account-validation')
+
 
 // Deliver Login View
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
@@ -9,6 +11,12 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin))
 // Deliver Registration View
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
 
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+// Process the registration data
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+  )
 
 module.exports = router
