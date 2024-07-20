29f3024d50b3bc02ff76c3ff4eaaeb2f385e40a1
Johnathon Staples <StapesJohnathonA@gmail.com>
2024-07-20 14:50:30 -0600

    add class and inventory working

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,13 +1,26 @@
-// Needed Resources 
-const express = require("express")
-const router = new express.Router() 
-const accountController = require("../controllers/accountController")
-const utilities = require("../utilities")
-
+// Needed Resources
+const express = require("express");
+const router = new express.Router();
+const accountController = require("../controllers/accountController");
+const utilities = require("../utilities");
+const regValidate = require("../utilities/account-validation");
 // Route to build account view
-router.get("/login", utilities.handleErrors(accountController.buildLogin))
-// Route to build registration view
-router.get("/registration", utilities.handleErrors(accountController.buildRegister))
+router.get("/login", utilities.handleErrors(accountController.buildLogin));
+router.get("/registration", utilities.handleErrors(accountController.buildRegister));
+router.post(
+  "/register",
+  regValidate.registrationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+);
+// Process the login attempt
+router.post(
+  "/login",
+  regValidate.loginRules(),
+  regValidate.checkLoginData,
+  (req, res) => {
+    res.status(200).send('login process')
+  }
+)
 
-router.post("/register", utilities.handleErrors(accountController.registerAccount))
 module.exports = router;
