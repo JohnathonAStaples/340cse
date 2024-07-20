174098bf3c96c5895999324a32413f01b91c115b
Johnathon Staples <StapesJohnathonA@gmail.com>
2024-07-20 09:54:49 -0600

    week 1

--- server.js
+++ server.js
@@ -10,89 +10,22 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-const baseController = require("./controllers/baseController")
-const inventoryRoute = require("./routes/inventoryRoute")
-const utilities = require("./utilities")
-const session = require("express-session")
-const pool = require('./database/')
-const bodyParser = require("body-parser")
 
 /* ***********************
  * View Engine and Templates
  *************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
-
-/* ***********************
- * Middleware
- * ************************/
-app.use(session({
-  store: new (require('connect-pg-simple')(session))({
-    createTableIfMissing: true,
-    pool,
-  }),
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  name: 'sessionId',
-}))
-
-// Express Messages Middleware
-app.use(require('connect-flash')())
-app.use(function(req, res, next){
-  res.locals.messages = require('express-messages')(req, res)
-  next()
-})
-
-// bodyParser middleware
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout"); // Not at view root
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
-
-/* ***********************
- * Index route
- *************************/
-app.get("/", utilities.handleErrors(baseController.buildHome))
-
-/* ***********************
- * Inventory routes
- *************************/
-app.use("/inv", utilities.handleErrors(inventoryRoute))
-
-/* ***********************
- * Account Routes
- *************************/
-app.use("/account", require("./routes/accountRoute"))
-
-/* ***********************
- * File Not Found Route - must be last route in list
- *************************/
-app.use(async (req, res, next) => {
-  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
-})
-
-/* ***********************
-* Express Error Handler
-* Place after all other middleware
-*************************/
-app.use(async (err, req, res, next) => {
-  let nav = await utilities.getNav()
-  console.error(`Error at: "${req.originalUrl}": ${err.message} ${err.status}`)
-  if(err.status == 404) {message = err.message
-  } else {
-    message = "Oh no! There was a crash. Maybe try a differnt route?"
-  }
-  res.render("errors/error", {
-    title: err.status || 'Server Error',
-    message,
-    nav
+//Index route
+app.get("/", function(request, response) {
+    response.render("index", {title: "Home"})
   })
-})
 
 /* ***********************
  * Local Server Information
